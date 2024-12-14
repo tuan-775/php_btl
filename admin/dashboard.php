148f6b736f37dd98568ffd729ed8267198776609
Quang Tuấn Phạm <phamquangtuan.contact@gmail.com>
2024-12-15 00:35:33 +0700

    update db

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -22,24 +22,24 @@ require '../db.php';
 <body>
     <header>
         <h1>Quản trị</h1>
+        <main>
+            <div>
+                <a href="../index.php" class="back-btn">Quay lại Trang chủ</a>
+            </div>
+            <div>
+                <a href="product_list.php" class="menu-btn">Danh sách sản phẩm</a>
+            </div>
+            <div>
+                <a href="inventory.php" class="menu-btn">Thống kê tồn kho</a>
+            </div>
+            <div>
+                <a href="revenue.php" class="menu-btn">Quản lý doanh thu</a>
+            </div>
+            <div>
+                <a href="manage_users.php" class="menu-btn">Quản lý người dùng</a>
+            </div>
+        </main>
     </header>
-    <main>
-        <div>
-            <a href="../index.php" class="back-btn">Quay lại Trang chủ</a>
-        </div>
-        <div>
-            <a href="product_list.php" class="menu-btn">Danh sách sản phẩm</a>
-        </div>
-        <div>
-            <a href="inventory.php" class="menu-btn">Thống kê tồn kho</a>
-        </div>
-        <div>
-            <a href="revenue.php" class="menu-btn">Quản lý doanh thu</a>
-        </div>
-        <div>
-            <a href="manage_users.php" class="menu-btn">Quản lý người dùng</a>
-        </div>
-    </main>
 </body>
 
 </html>
--- db/rabitydb.sql
+++ db/rabitydb.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 13, 2024 at 09:24 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th12 14, 2024 lúc 06:33 PM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `rabitydb`
+-- Cơ sở dữ liệu: `rabitydb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cart`
+-- Cấu trúc bảng cho bảng `cart`
 --
 
 CREATE TABLE `cart` (
@@ -37,16 +37,17 @@ CREATE TABLE `cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `cart`
+-- Đang đổ dữ liệu cho bảng `cart`
 --
 
 INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`, `created_at`, `size`) VALUES
-(9, 0, 5, 1, '2024-11-24 17:57:28', NULL);
+(9, 0, 5, 1, '2024-11-24 17:57:28', NULL),
+(89, 5, 2, 1, '2024-12-14 10:27:35', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Cấu trúc bảng cho bảng `orders`
 --
 
 CREATE TABLE `orders` (
@@ -68,7 +69,7 @@ CREATE TABLE `orders` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `orders`
+-- Đang đổ dữ liệu cho bảng `orders`
 --
 
 INSERT INTO `orders` (`id`, `user_name`, `phone`, `address`, `product_id`, `product_name`, `price`, `quantity`, `total_price`, `created_at`, `payment_method`, `shipping_method`, `shipping_cost`, `bank`, `user_id`) VALUES
@@ -127,12 +128,15 @@ INSERT INTO `orders` (`id`, `user_name`, `phone`, `address`, `product_id`, `prod
 (53, 'Nguyễn Đình Tuấn', '0836 998 775', '11111', 1, 'Áo thun tay dài', 100000.00, 2, 250000.00, '2024-12-13 06:23:24', 'bank_transfer', 'express', 50000, 'MB', 1),
 (54, 'Nguyễn Đình Tuấn', '0836 998 775', '11111', 1, 'Áo thun tay dài', 100000.00, 1, 130000.00, '2024-12-13 06:25:50', 'bank_transfer', 'standard', 30000, 'VCB', 1),
 (55, 'Nguyễn Đình Tuấn', '0836 998 775', '11111', 1, 'Áo thun tay dài', 100000.00, 2, 250000.00, '2024-12-13 07:38:47', 'bank_transfer', 'express', 50000, 'VCB', 1),
-(61, 'Nguyễn Đình Tuấn', '0836 998 775', '11111', 2, 'Bộ đồ thun ngắn tay', 120000.00, 1, 150000.00, '2024-12-13 07:58:23', 'bank_transfer', 'standard', 30000, 'MB', 3);
+(61, 'Nguyễn Đình Tuấn', '0836 998 775', '11111', 2, 'Bộ đồ thun ngắn tay', 120000.00, 1, 150000.00, '2024-12-13 07:58:23', 'bank_transfer', 'standard', 30000, 'MB', 3),
+(62, 'test1', '0369585104', 'Hà Nội', 7, 'Áo thun dài tay bé gái Rabity', 123123.00, 10, 1281230.00, '2024-12-13 16:16:18', 'bank_transfer', 'express', 50000, 'Agribank', 1),
+(63, 'tuan test', '0123456789', 'Hà Nội', 6, 'Mũ lưỡi trai bé trai', 99999999.99, 10, 99999999.99, '2024-12-13 18:20:53', 'bank_transfer', 'standard', 30000, 'MB', 1),
+(64, 'tuan test', '0123456789', 'Hà Nội', 2, 'Bộ đồ thun ngắn tay', 120000.00, 8, 1010000.00, '2024-12-14 10:27:18', 'COD', 'express', 50000, '', 5);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Cấu trúc bảng cho bảng `products`
 --
 
 CREATE TABLE `products` (
@@ -153,24 +157,24 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `products`
+-- Đang đổ dữ liệu cho bảng `products`
 --
 
 INSERT INTO `products` (`id`, `product_code`, `category`, `category_name`, `name`, `image`, `description`, `price`, `created_at`, `sale_percentage`, `stock`, `cost_price`, `sale_price`, `sold_quantity`) VALUES
 (1, '001', 'Bé trai,BST Đồ Bộ Mặc Nhà', 'Áo', 'Áo thun tay dài', 'Screenshot 2024-12-12 211557.png', 'Nội dung đang được cập nhật', 100000.00, '2024-11-24 14:56:38', -10, 32, 10000.00, NULL, 17),
-(2, '002', 'Bé trai', 'Đồ bộ', 'Bộ đồ thun ngắn tay', '../uploads/Screenshot 2024-11-23 182601.png', 'Mô tả sản phẩm\r\nĐồ bộ thun bé trai sẽ là outfits tiện lợi cho mẹ và bé, với kiểu dáng áo thun, quần short cá tính, năng động giúp bé thoải mái vận động. Với những mẫu đồ bộ tính dụng cao bé có thể mặc mặc ở nhà, đi học, đi chơi,... \r\n\r\n1. Thông tin Bộ thun ngắn tay Mickey bé trai Rabity 560.001\r\n- Chất liệu 95% vải cotton và 5% spandex thoáng mát, co giãn và an toàn cho làn da của bé\r\n\r\n- Loại sản phẩm: Đồ bộ bé trai\r\n\r\n- Phù hợp với bé trai cân nặng từ 11-21kg, từ 2-6 tuổi\r\n\r\n- Bộ thun ngắn tay in hình Mickey bản quyền Disney, hình in sắc nét và màu sắc hài hòa\r\n\r\n \r\n\r\n2. Thông tin chi tiết Bộ thun ngắn tay Mickey bé trai Rabity 560.001\r\nBộ thun ngắn tay bé trai form vừa vặn thoải mái. Kiểu dáng dễ phối nhiều outfit lịch sự cho bé diện đi học, đi tiệc hay xuống phố. Sản phẩm đạt chứng nhận Oeko-Tex 100 an toàn cho da trẻ em.', 120000.00, '2024-11-24 15:11:40', 0, 49, NULL, NULL, 1),
+(2, '002', 'Bé trai', 'Đồ bộ', 'Bộ đồ thun ngắn tay', '../uploads/Screenshot 2024-11-23 182601.png', 'Mô tả sản phẩm\r\nĐồ bộ thun bé trai sẽ là outfits tiện lợi cho mẹ và bé, với kiểu dáng áo thun, quần short cá tính, năng động giúp bé thoải mái vận động. Với những mẫu đồ bộ tính dụng cao bé có thể mặc mặc ở nhà, đi học, đi chơi,... \r\n\r\n1. Thông tin Bộ thun ngắn tay Mickey bé trai Rabity 560.001\r\n- Chất liệu 95% vải cotton và 5% spandex thoáng mát, co giãn và an toàn cho làn da của bé\r\n\r\n- Loại sản phẩm: Đồ bộ bé trai\r\n\r\n- Phù hợp với bé trai cân nặng từ 11-21kg, từ 2-6 tuổi\r\n\r\n- Bộ thun ngắn tay in hình Mickey bản quyền Disney, hình in sắc nét và màu sắc hài hòa\r\n\r\n \r\n\r\n2. Thông tin chi tiết Bộ thun ngắn tay Mickey bé trai Rabity 560.001\r\nBộ thun ngắn tay bé trai form vừa vặn thoải mái. Kiểu dáng dễ phối nhiều outfit lịch sự cho bé diện đi học, đi tiệc hay xuống phố. Sản phẩm đạt chứng nhận Oeko-Tex 100 an toàn cho da trẻ em.', 120000.00, '2024-11-24 15:11:40', 0, 41, NULL, NULL, 9),
 (3, '003', 'Bé gái', 'Đầm váy', 'Đầm váy thô cổ sen ngắn tay bé gái', '../uploads/Screenshot 2024-11-23 183607.png', 'Đầm váy thô cổ sen ngắn tay bé gái Rabity 93113\r\n1. Thông tin Đầm váy thô cổ sen ngắn tay bé gái Rabity 93113\r\n- Chất liệu 100% thoáng mát, mềm mịn dễ chịu\r\n\r\n- Loại sản phẩm: Đầm váy bé gái\r\n\r\n- Phù hợp với bé gái cân nặng từ 11-30kg, từ 2-10 tuổi\r\n\r\n- Đầm thô ngắn tay họa tiết hoa đáng yêu, màu sắc nổi bật, thời trang cho bé diện thật nổi bật.', 1500000.00, '2024-11-24 15:13:08', 50, 50, NULL, NULL, 0),
 (4, '004', 'Bé gái', 'Áo', 'Chia sẻ Áo thun ngắn tay bé gái', '../uploads/Screenshot 2024-11-24 221346.png', 'Áo thun là một outfits tiện lợi cho mẹ và bé, với kiểu dáng áo thun cá tính, năng động sẽ giúp bé thoải mái vận động. Ba mẹ có thể phối cho bé áo thun với những quần khác nhau như quần nỉ, quần dài, quần thun,... cho bé mặc nhiều dịp đi học, đi chơi, đi học, đi tiệc,...\r\n\r\n1. Đặc điểm nổi bật Áo thun ngắn tay bé gái 900.076\r\n- Chất liệu 100% vải cotton xước thông thoáng, mát mẻ và an toàn cho làn da của bé Cotton\r\n\r\n- Loại sản phẩm: Áo bé gái, Áo thun bé gái\r\n\r\n- Phù hợp với bé gái cân nặng từ 11 - 35kg, từ 2 - 12 tuổi\r\n\r\n- Áo thun ngắn tay in hình trái dưa hấu dễ thương, năng động và sắc nét.\r\n\r\n2. Thông tin chi tiết Áo thun ngắn tay bé gái 900.076\r\nÁo thun ngắn tay bé gái form vừa vặn thoải mái. Kiểu dáng dễ phối nhiều outfit lịch sự cho bé diện đi học, đi tiệc hay xuống phố. Sản phẩm đạt chứng nhận Oeko-Tex 100 an toàn cho da trẻ em.', 1510325.00, '2024-11-24 15:14:17', 0, 49, NULL, NULL, 0),
 (5, '005', 'Bé gái', 'Đồ bộ', 'Đồ bộ thun tăm dài tay bé gái Rabity', '../uploads/Screenshot 2024-11-24 221446.png', 'Thời tiết thay đổi liên tục, những ngày lạnh hay nhiệt độ thấp vào ban đêm, đồ bộ dài tay cho bé là cứu tinh lúc này. Chất thun tăm được dệt từ sợi cotton co giãn thoải mái, bé mặc thích thú cả ngày.\r\n\r\n1. Đặc điểm nổi bật Đồ bộ thun tăm dài tay bé gái Rabity 961.001\r\nChất liệu: Với thiết kế 100% vải cotton nhẹ thoáng, êm mịn và an toàn cho làn da bé.\r\nĐộ tuổi, cân nặng: phù hợp cho bé từ 2 - 6 tuổi, từ 11-21kg.\r\nLoại sản phẩm: Đồ bộ bé gái, đồ bộ dài tay bé gái.\r\nÁo tay dài quần dài, họa tiết bông hoa nhỏ xinh xắn.\r\n2. Thông tin chi tiết Đồ bộ thun tăm dài tay bé gái Rabity 961.001\r\nBộ thun tăm dài tay bé gái kiểu dáng điệu đà, màu sắc hồng nhạt, vàng nhạt dịu dàng cho các bé gái. Sản phẩm thích hợp cho bé mặc trong mùa thu đông hoặc khi thời tiết lạnh, mang đến cảm giác ấm áp và thoải mái khi mặc.', 99999999.99, '2024-11-24 15:15:08', 0, 50, NULL, NULL, 0),
-(6, '006', 'Bé trai', 'Phụ kiện', 'Mũ lưỡi trai bé trai', '../uploads/Screenshot 2024-11-24 221544.png', 'Freesize', 99999999.99, '2024-11-24 15:16:11', 0, 49, NULL, NULL, 0),
-(7, '007', 'Bé gái', 'Áo', 'Áo thun dài tay bé gái Rabity', '../uploads/Screenshot 2024-11-26 125423.png', 'Nội dung đang được cập nhật', 123123.00, '2024-11-26 05:54:53', 0, 50, NULL, NULL, 0),
+(6, '006', 'Bé trai', 'Phụ kiện', 'Mũ lưỡi trai bé trai', '../uploads/Screenshot 2024-11-24 221544.png', 'Freesize', 99999999.99, '2024-11-24 15:16:11', 0, 39, NULL, NULL, 10),
+(7, '007', 'Bé gái', 'Áo', 'Áo thun dài tay bé gái Rabity', '../uploads/Screenshot 2024-11-26 125423.png', 'Nội dung đang được cập nhật', 123123.00, '2024-11-26 05:54:53', 0, 40, NULL, NULL, 10),
 (9, '011', 'Bé trai', 'Quần', 'quần', 'Screenshot 2024-11-23 000410.png', '2ewrgthgth', 100000.00, '2024-12-05 07:06:40', 10, 44, 10000.00, NULL, 0),
 (10, '014', 'Bé gái,BST Thu Đông', 'Đồ bộ', 'Quần nỉ dài bé trai', 'Screenshot 2024-12-05 171612.png', 'Quần nỉ là sự kết hợp hoàn hảo giữa phong cách năng động và tiện dụng. Quần nỉ không chỉ phù hợp cho những buổi dạo phố, mà còn là lựa chọn lý tưởng cho các bé khi tham gia các hoạt động ngoại khóa, vui chơi thể thao. Hơn nữa, với kiểu dáng thời trang và màu sắc phong phú, quần nỉ chắc chắn sẽ giúp các bé tự tin và nổi bật trong mọi hoàn cảnh\r\n\r\n1. Đặc điểm nổi bật Quần nỉ dài bé trai \r\nNhóm sản phẩm: Quần bé trai; Quần dài bé trai\r\nChất liệu: 95% cotton 5% spandex an toàn và thoáng mát cho da của bé\r\nSize: Phù hợp với bé trai cân nặng từ 14 - 35kg, từ 4 - 12 tuổi\r\n \r\n\r\n2. Chất liệu Quần nỉ dài bé trai \r\nQuần dài bé trai form vừa vặn thoải mái. Kiểu dáng dễ phối nhiều outfit lịch sự cho bé diện đi học, đi tiệc hay xuống phố. Sản phẩm đạt chứng nhận Oeko-Tex 100 an toàn cho da trẻ em.\r\n\r\n', 100000.00, '2024-12-05 10:17:27', 5, 14, 10000.00, NULL, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_categories`
+-- Cấu trúc bảng cho bảng `product_categories`
 --
 
 CREATE TABLE `product_categories` (
@@ -182,7 +186,7 @@ CREATE TABLE `product_categories` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product_images`
+-- Cấu trúc bảng cho bảng `product_images`
 --
 
 CREATE TABLE `product_images` (
@@ -192,7 +196,7 @@ CREATE TABLE `product_images` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `product_images`
+-- Đang đổ dữ liệu cho bảng `product_images`
 --
 
 INSERT INTO `product_images` (`id`, `product_id`, `image_path`) VALUES
@@ -204,31 +208,36 @@ INSERT INTO `product_images` (`id`, `product_id`, `image_path`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Cấu trúc bảng cho bảng `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `fullname` varchar(200) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `gender` varchar(10) NOT NULL,
   `role` enum('admin','user') DEFAULT 'user',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Đang đổ dữ liệu cho bảng `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`, `created_at`) VALUES
-(1, 'tuan1', 'xmeosadx@gmail.com', '$2y$10$..qwYnX8sNPFB5/CjcPMMO1UxCnYy3udkvKAT8zDuXY2c4PldG.Dq', 'user', '2024-11-24 13:34:32'),
-(2, 'admin', 'xmeo2612x@gmail.com', '$2y$10$.STfEayHecZFksdKU.OAIup92zbneMWwJ1goW.r0DetqvFxLVJ1fG', 'admin', '2024-11-24 13:56:47'),
-(3, 'tuan2', 'Farmx12@zz.zz', '$2y$10$.0afaOuXOA066qrwu/QjgOnKVC5PvzCPNXvmhuUoy8Af0aenzt1.m', 'user', '2024-12-03 10:47:36');
+INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`, `gender`, `role`, `created_at`) VALUES
+(1, '', 'tuan1', 'xmeosadx@gmail.com', '$2y$10$..qwYnX8sNPFB5/CjcPMMO1UxCnYy3udkvKAT8zDuXY2c4PldG.Dq', '', 'user', '2024-11-24 13:34:32'),
+(2, '', 'admin', 'xmeo2612x@gmail.com', '$2y$10$.STfEayHecZFksdKU.OAIup92zbneMWwJ1goW.r0DetqvFxLVJ1fG', '', 'admin', '2024-11-24 13:56:47'),
+(3, '', 'tuan2', 'Farmx12@zz.zz', '$2y$10$.0afaOuXOA066qrwu/QjgOnKVC5PvzCPNXvmhuUoy8Af0aenzt1.m', '', 'user', '2024-12-03 10:47:36'),
+(4, 'Phạm Quang Tuấn', 'quangtuan', 'quangtuan@gmail.com', '$2y$10$6tDmsLa7oLK.phCaocDC/uHZowoBKxZBc74Vymk6XeWZOe39.wZsq', 'Nam', 'user', '2024-12-14 10:00:46'),
+(5, 'Trần Phương Ly', 'phuonglyxinhgai', 'may_lily@gmail.com', '$2y$10$NyJ5MIYMgC/boQKce4zk0evQCm/Zh6HQ/S2kSeAuOBNoDcNxdxtry', 'Nữ', 'user', '2024-12-14 10:14:51'),
+(6, 'Phạm Quang Tuấn1', 'quangtuanxautrai', 'ptuan2594@gmail.com', '$2y$10$fz/96XXY/qB3swrJgnKxQ.4QqygfTSyfBgr1f36n/ywakC6tlM37S', 'Nam', 'user', '2024-12-14 16:41:25');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_profiles`
+-- Cấu trúc bảng cho bảng `user_profiles`
 --
 
 CREATE TABLE `user_profiles` (
@@ -243,7 +252,7 @@ CREATE TABLE `user_profiles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `user_profiles`
+-- Đang đổ dữ liệu cho bảng `user_profiles`
 --
 
 INSERT INTO `user_profiles` (`id`, `user_id`, `gender`, `birthdate`, `phone`, `address`, `created_at`, `updated_at`) VALUES
@@ -256,45 +265,45 @@ INSERT INTO `user_profiles` (`id`, `user_id`, `gender`, `birthdate`, `phone`, `a
 (7, 1, 'Nam', '2004-06-26', '0836 998 775', 'Vũ Thư, Thái Bình', '2024-12-13 07:24:45', '2024-12-13 07:24:45');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `cart`
+-- Chỉ mục cho bảng `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `orders`
+-- Chỉ mục cho bảng `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_id` (`user_id`);
 
 --
--- Indexes for table `products`
+-- Chỉ mục cho bảng `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `product_categories`
+-- Chỉ mục cho bảng `product_categories`
 --
 ALTER TABLE `product_categories`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `product_images`
+-- Chỉ mục cho bảng `product_images`
 --
 ALTER TABLE `product_images`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `users`
+-- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -302,88 +311,88 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `user_profiles`
+-- Chỉ mục cho bảng `user_profiles`
 --
 ALTER TABLE `user_profiles`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `cart`
+-- AUTO_INCREMENT cho bảng `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT cho bảng `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
--- AUTO_INCREMENT for table `products`
+-- AUTO_INCREMENT cho bảng `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `product_categories`
+-- AUTO_INCREMENT cho bảng `product_categories`
 --
 ALTER TABLE `product_categories`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `product_images`
+-- AUTO_INCREMENT cho bảng `product_images`
 --
 ALTER TABLE `product_images`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT cho bảng `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `user_profiles`
+-- AUTO_INCREMENT cho bảng `user_profiles`
 --
 ALTER TABLE `user_profiles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `cart`
+-- Các ràng buộc cho bảng `cart`
 --
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `orders`
+-- Các ràng buộc cho bảng `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `product_categories`
+-- Các ràng buộc cho bảng `product_categories`
 --
 ALTER TABLE `product_categories`
   ADD CONSTRAINT `product_categories_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
--- Constraints for table `product_images`
+-- Các ràng buộc cho bảng `product_images`
 --
 ALTER TABLE `product_images`
   ADD CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `user_profiles`
+-- Các ràng buộc cho bảng `user_profiles`
 --
 ALTER TABLE `user_profiles`
   ADD CONSTRAINT `user_profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
